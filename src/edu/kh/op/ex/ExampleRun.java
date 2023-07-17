4480d1736cb0cd8ea13db4f2e4a5feac112b706e
unknown <vegayes@naver.com>
2023-07-17 09:41:29 +0900

    hello

--- src/edu/kh/op/ex/ExampleRun.java
+++ src/edu/kh/op/ex/ExampleRun.java
@@ -1,8 +1,15 @@
 package edu.kh.op.ex;
 
-public class ExampleRun {
+public class ExampleRun { // 코드 실행용 클래스
 	
+	// 메인 메서드 필수 작성 
 	public static void main(String[] args) {
+		
+		// OpExample  생성 
+		OpExample ex= new OpExample(); 
+		// 같은 패키지 안에 있어서 import 따로 안해도 됨.(에러 X) 
+		
+		ex.ex1();
 		
 		
 	}
--- src/edu/kh/op/ex/OpExample.java
+++ src/edu/kh/op/ex/OpExample.java
@@ -1,9 +1,16 @@
 package edu.kh.op.ex;
 
-public class OpExample {
-	
-	public void 
+public class OpExample { // 예제 코드 작성용 클래스
 	
 	
+	// ex1() method : 객체 지향 프로그래밍에서 객체와 관련된 함수
+	// -> OpExample이 가지고 있는 기능 중 ex1()이라는 기능
+	public void ex1() { // 반환하는 값이 없을 떄?
+		
+		System.out.println("예제 코드 작성 실행 1");
+		System.out.println("예제 코드 작성 실행 2");
+		System.out.println("예제 코드 작성 실행 3");
+
+	}
 
 }
